5d2d9cbe19f3813bad483d92f15a291fd9869a3a
waxtears <waxtears>
2021-05-29 22:04:00 +0800

    modeone

--- ModeOne/go.mod
+++ ModeOne/go.mod
@@ -3,8 +3,15 @@ module github.com/waxtears/waxtear.com/ModeOne
 go 1.16
 
 require (
+	github.com/asim/go-micro/plugins/registry/consul/v3 v3.0.0-20210523073820-acc3f5479f48
 	github.com/asim/go-micro/v3 v3.5.1
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/golang/protobuf v1.4.3
-	github.com/micro/micro/v3 v3.2.1
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.25.0
 )
--- ModeOne/main.go
+++ ModeOne/main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"fmt"
 
+	"github.com/asim/go-micro/plugins/registry/consul/v3"
 	"github.com/asim/go-micro/v3"
 	"github.com/asim/go-micro/v3/logger"
+	"github.com/asim/go-micro/v3/registry"
+
+	//"github.com/asim/nitro/v3/registry"
+	//"github.com/asim/nitro-plugins/registry/consul/v3"
 	"github.com/waxtears/waxtear.com/ModeOne/handler"
 	"github.com/waxtears/waxtear.com/ModeOne/proto/ModelOneProto"
 )
 
 func main() {
+	consulRegistry := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"127.0.0.1:8500",
+		}
+	})
+
 	// Create service
 	svr := micro.NewService(
 		micro.Name("modeone"),
 		micro.Version("latest"),
+		micro.Address("127.0.0.1:8082"),
+		micro.Registry(consulRegistry),
 	)
 
 	svr.Init()
